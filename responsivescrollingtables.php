9a2e2848f8f849a9329fbd5a0f10a9b2ef854bff
TJDixonLimited <TJDixonLimited@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 09:27:49 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/responsive-scrolling-tables/tags/1.0@959846 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ responsivescrollingtables.php
@@ -0,0 +1,26 @@
+﻿<?php
+/*
+Plugin Name: Responsive Scrolling Tables
+Description: RST detects when tables are bigger then their containers and makes them scroll instead of flowing over the boundary of the container.
+Version: 1.0.0
+Author: T J Dixon Limited
+Author URI: http://www.tjdixon.com/
+Plugin URI: http://www.tjdixon.com/
+License: GPLv3
+*/
+
+defined('WPINC') or die();
+
+function addRSTJS()
+{
+	wp_enqueue_script(
+		'custom_script',
+		plugins_url('/js/responsivescrollingtables.js', __FILE__)
+	);
+}
+echo __FILE__;
+
+//add_action('wp_enqueue_scripts', addRSTJS());
+
+add_action('wp_head', 'addRSTJS');
+?>
