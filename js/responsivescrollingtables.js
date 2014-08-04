e98bc1aba6a757a1f295e5fb074985dfd65fed3b
TJDixonLimited <TJDixonLimited@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 09:22:28 +0000

    Adding first version of Responsive Scrolling Tables
    git-svn-id: https://plugins.svn.wordpress.org/responsive-scrolling-tables/trunk@959843 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/responsivescrollingtables.js
@@ -0,0 +1,37 @@
+/**
+ * @package     ResponsiveScrollingTables
+ *
+ * @copyright   Copyright (C) 2014 T J Dixon Limited. All rights reserved. http://www.tjdixon.com/
+ * @license     Licensed under the GNU GPLv3 http://www.gnu.org/licenses/gpl.html or later
+ */
+
+function responsiveTables() 
+{
+    var tables = document.querySelectorAll("table");
+    for (var i = 0; i < tables.length; i++) 
+	{
+        if (tables[i].scrollWidth > tables[i].parentNode.clientWidth && (tables[i].parentNode.tagName.toLowerCase() != "div" || tables[i].parentNode.getAttribute("data-responsive") != "res-div"))
+		{
+            var d = document.createElement("div");
+			var parent = tables[i].parentNode;
+            d.appendChild(tables[i].cloneNode(true));
+            d.setAttribute("style","overflow-x:scroll;");
+			d.setAttribute("data-responsive","res-div");
+            parent.replaceChild(d,tables[i]);
+        }
+		else if (tables[i].scrollWidth <= tables[i].parentNode.clientWidth && (tables[i].parentNode.tagName.toLowerCase() == "div" && tables[i].parentNode.getAttribute("data-responsive") == "res-div"))
+		{
+			var tablesParent = tables[i].parentNode;
+			var parentsParent = tablesParent.parentNode;
+			parentsParent.replaceChild(tables[i].cloneNode(true), tablesParent);
+		}
+    }
+}
+
+window.addEventListener('resize', function (e) { responsiveTables(); });
+
+document.onreadystatechange = function () {
+    if (document.readyState == 'complete') {
+        responsiveTables();
+    }
+};
